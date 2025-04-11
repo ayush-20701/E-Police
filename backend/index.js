86e2933ff2d599ad4686c1777d929cf7d555dea5
Amaanuddin05 <amaanuddin0501@gmail.com>
2025-04-11 16:19:42 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello ')
 })
-
+app.get('/users', (req, res) => {
+  res.send('users ')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
